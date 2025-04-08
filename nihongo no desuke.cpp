a371bddaa9deb30d86289f6d40a482f87c1c4963
zukaschikume <99959219+zukaschikume@users.noreply.github.com>
2025-04-08 08:43:12 +0200

    Update nihongo no desuke.cpp

--- nihongo no desuke.cpp
+++ nihongo no desuke.cpp
@@ -3,20 +3,44 @@
 
 using namespace std;
 
-double f(double x) {
-    return x * x - 4;  // Przykład: x^2 - 4 = 0, pierwiastki to x = -2 i x = 2
+double funkcja(double x) {
+    return x * x - 4; 
 }
 
 double bisection(double a, double b, double epsilon) {
-    while ((b - a) / 2 > epsilon) {
-        double c = (a + b) / 2;
-        if (f(c) == 0) return c;
-        (f(a) * f(c) < 0) ? b = c : a = c;  // Warunek zmiany przedziału
+    // Krok K1 i K2: sprawdzamy czy pierwiastek nie znajduje się na granicach przedziału
+    if (funkcja(a) == 0.0) {
+        return a;  // Jeśli f(a) = 0, to a jest pierwiastkiem
     }
-    return (a + b) / 2;
+    if (funkcja(b) == 0.0) {
+        return b;  // Jeśli f(b) = 0, to b jest pierwiastkiem
+    }
+
+    // Krok K3: dopóki szerokość przedziału jest większa niż epsilon
+    while ((b - a) > epsilon) {
+        // Krok K4: obliczamy środek przedziału
+        double srodek = (a + b) / 2.0;
+
+        // Krok K5: jeżeli f(srodek) = 0 to zwracamy środek
+        if (funkcja(srodek) == 0.0) {
+            return srodek;
+        }
+
+        // Krok K6: sprawdzamy w której połowie przedziału znajduje się pierwiastek
+        if (funkcja(a) * funkcja(srodek) < 0) {
+            b = srodek;  // Pierwiastek znajduje się w lewej części przedziału
+        } else {
+            a = srodek;  // Pierwiastek znajduje się w prawej części przedziału
+        }
+    }
+
+    return (a + b) / 2.0;
 }
 
 int main() {
-    cout << "Pierwiastek to: " << bisection(0, 3, 1e-6) << endl;
+    double a = 3, b = 3, epsilon = 0.001; 
+    double pierwiastek = bisection(a, b, epsilon);
+
+    cout << "Znalezione miejsce zerowe wynosi: " << pierwiastek << endl;
     return 0;
 }
